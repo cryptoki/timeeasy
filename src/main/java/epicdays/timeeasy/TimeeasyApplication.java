48f7a30d19354d538a146ba61777fca637e42502
Lars Gentsch <lars@gentsch.eu>
2018-01-03 18:12:38 +0100

    First application setup.

---
+++ src/main/java/epicdays/timeeasy/TimeeasyApplication.java
@@ -0,0 +1,12 @@
+package epicdays.timeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeeasyApplication.class, args);
+	}
+}
---
+++ src/test/java/epicdays/timeeasy/TimeeasyApplicationTests.java
@@ -0,0 +1,16 @@
+package epicdays.timeeasy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeeasyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
