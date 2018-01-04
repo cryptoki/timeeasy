8cc4355293ace2472db6a6a3818c0350f3b982d9
Steffen Haase <sh@java-club.de>
2018-01-04 10:48:16 +0100

    swagger and a bit of stuff

--- src/main/java/epicdays/timeeasy/controller/ActivityController.java
+++ src/main/java/epicdays/timeeasy/controller/ActivityController.java
@@ -1,9 +1,24 @@
 package epicdays.timeeasy.controller;
 
+import io.swagger.annotations.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RequestMapping("/activities")
 @RestController
 public class ActivityController {
+
+    @ApiOperation(value = "get all activities", code = 201)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<Collection<ActivityVO>> createDeliverer() {
+        return null;
+    }
 }
