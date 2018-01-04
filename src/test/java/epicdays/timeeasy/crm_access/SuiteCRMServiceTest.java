e23484a2c0a23880ff96bb7b274e7060251b89c9
Lars Gentsch <lars@gentsch.eu>
2018-01-04 10:30:58 +0100

    First raw test for SuiteCRm access.

---
+++ src/test/java/epicdays/timeeasy/crm_access/SuiteCRMServiceTest.java
@@ -0,0 +1,24 @@
+package epicdays.timeeasy.crm_access;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+@RunWith(JUnit4.class)
+public class SuiteCRMServiceTest {
+
+    SuiteCRMService suiteCRMService = new SuiteCRMService();
+
+    @Test @Ignore
+    public void save_a_time_record_in_SuiteCRM() {
+        TimeRecord timeRecord = new TimeRecord("klaus","geheim", new Date(System.currentTimeMillis()), 8);
+        suiteCRMService.bookTimeRecord(timeRecord);
+        assertTrue(true);
+    }
+}
